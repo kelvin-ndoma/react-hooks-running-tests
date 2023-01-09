434f619837ccd9a1a70281b79d53e355c6f0e578
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-09 12:37:37 +0300

    completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-function Article() {
+function Article(props) {
   return (
     <div>
-      {/* please pass this test */}
+       please pass this test
     </div>
   );
 }
-
 export default Article;
